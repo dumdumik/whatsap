04605436e9b793970341d8c9e3f09b0529d554d6
dumdumik <fjdvgt149@gmail.com>
2025-02-05 19:07:27 +0700

    sbilsya

--- js/main.js
+++ js/main.js
@@ -12,13 +12,31 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        ubratOnCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            } else {
+                alert("Бро, корзина и так пуста куда минусуешь");
+            }
+        }
     }
 })
