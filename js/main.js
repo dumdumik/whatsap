327c4ac6bc5ddb75dee94982e8b85ce723818e55
dumdumik <fjdvgt149@gmail.com>
2025-02-05 14:00:12 +0700

    bratan

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks"
-
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        link: "More products like this",
     }
 })
